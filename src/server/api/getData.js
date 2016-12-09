d938a70cdf3c6eebe91389235882f34e2b52072e
Travis Sheppard <tshepp@stanford.edu>
2016-12-09 09:40:20 -0800

    start getting dyanic data without saving locally

--- src/server/api/getData.js
+++ src/server/api/getData.js
@@ -1,16 +1,22 @@
-let chrom1 = require('./data/1.json');
-let chrom2 = require('./data/2.json');
-let chrom3 = require('./data/3.json');
+var fetch = require('node-fetch');
 let simpleData = require('./data/22.json');
 
-let dataObj = {
-  '1': chrom1,
-  '2': chrom2,
-  '3': chrom3,
-};
+const BASE_URL = 'http://1kgenome.exascale.info/3d?m=normal&chr=';
+const SUFFIX = '&xstart=0&xend=15&zstart=0&zend=15&ystart=0&yend=15';
+
 
 module.exports = function getData (req, res) {
-	let chrom = req.params.chrom;
-	let data = dataObj[chrom] || simpleData;
-  res.send(data);
+  let chrom = req.params.chrom;
+  let url = BASE_URL + chrom + SUFFIX;
+  // fetch from 1k genome
+  fetch(url)
+    .then(function(apiRes) {
+      return apiRes.text();
+    }).then(function(body) {
+      let clean = body.replace(/\(|\)/g,'');
+      return res.send(clean);
+    });
+
+  // let data = dataObj[chrom] || simpleData;
+  // res.send(data);
 };
